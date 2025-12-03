da6057057be8a51c5553d22200b1cafec75b1b43
Christopher Edward Dawn <cedtwo@tutanota.com>
2025-12-03 20:50:35 +0800

    Improve TypedBound::split documentation

--- src/typed_bound.rs
+++ src/typed_bound.rs
@@ -5,17 +5,19 @@ pub trait TypedBound<const INDEX: usize, L, R>: Sized {
     /// Split the tuple.
     ///
     /// Tuples are split by providing either the leftmost elements `L`, the rightmost
-    /// elements `R`, or the left-inclusive index `Idx`.
+    /// elements `R`, or the left-inclusive index `INDEX`.
     /// # Example
     /// ```
     /// # use typed_tuple::TypedBound;
     /// // Get by type.
     /// let tuple = (0u8, 1u16, 2u32, 3u64, 4u128);
-    /// let (l, r): ((_, _, _), _) = tuple.split();
+    /// let (l, r): ((_, _, _), _) = tuple.split(); // Split the first three elements.
+    /// assert!(l, (0, 1, 2));
     ///
     /// // Get by index (left inclusive).
     /// let tuple = (0u8, 1u16, 2u32, 3u64, 4u128);
-    /// let (l, r) = TypedBound::<2, _, _>::split(tuple);
+    /// let (l, r) = TypedBound::<2, _, _>::split(tuple); // Split at index 2 (inclusive).
+    /// assert!(r, (3, 4));
     /// ```
     fn split(self) -> (L, R);
 }
